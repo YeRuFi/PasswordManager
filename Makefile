aadc4ac3f77ac22b84169791637e7cc39c7f9b5c
Jan Fischer <jan.fischer@tum.de>
2016-01-26 14:19:10 +0100

    [First simple write to Flash] Simple write and read to and from Flash using some address all in main. Read by using the miosix memDump function.
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ SUBDIRS := $(KPATH)
 ## List here your source files (both .s, .c and .cpp)
 ##
 SRC :=                                  \
-main.cpp
+main.cpp FlashDriver.cpp
 ##added button
 
 ##
--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <miosix.h>
 #include <stdio.h>
+#include <FlashDriver.h>
+#include <string>
+
 using namespace miosix;
+
 int main()
 {
     for(;;)
     {
-        ledOn();
-        Thread::sleep(1000);
-        ledOff();
-        Thread::sleep(1000);
-	printf("Hello! Wordl\n");
+	char test= 'p';
+	printf("This character will be written to address 0xFFFDF: %c\n", test);     
+	ledOn();      
+	FlashDriver& flashdriver = FlashDriver::instance();
+	flashdriver.write(0xFFFDF,test); //parameters are address to write to and char to write
+	ledOff();
+	printf("Character read at address 0xFFFDF: ");	
+	memDump((char *) 0xFFFDF, 1);
+        printf("\n");
+	Thread::sleep(1000);//testing
+	
     }
-	//iprintf("Hello! Wordl");
 }
