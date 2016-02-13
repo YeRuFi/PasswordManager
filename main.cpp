c214cd2231aa607e4d9d637851627760fdd4affc
Jan Fischer <jan.fischer@tum.de>
2016-02-13 20:08:18 +0100

    [Added correct addresses for Flash] Previous version still used wrong addresss. For Flash 0x080AAAAA must be used.
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- main.cpp
+++ main.cpp
@@ -11,18 +11,20 @@ bool writePassword(char c)
 {
 	//FlashDriver& flashdriver = FlashDriver::instance();
 	//startAddress= flashdriver.getSectorAddress(flashdriver.getStartSector());
-	//flashdriver.erase(flashdriver.getStartSector());	
-	return flashdriver.write(0xF8000,c); //parameters are address to write to and char to write
+	//flashdriver.erase(flashdriver.getSectorNumber(0xF8000)); //wrong address format?	
+	//flashdriver.erase(); //destroys os?
+	flashdriver.erase(flashdriver.getSectorNumber(0x080F8000)); //correct flash address
+	return flashdriver.write(0x080F8000,c); //parameters are address to write to and char to write
 }
 
 int main()
 {
 	char test= 'p';	
-	printf("Enter character to be written to address 0xF8000: ");     
+	printf("Enter character to be written to address 0x080F8000: ");     
 	scanf("%c", &test);	     
 	bool w = writePassword(test);
-	printf("Write result: %i,Character read at address 0xF8000: ", (int) w);	
-	memDump((char *) 0xF8000, 4);
+	printf("Write result: %i,Character read at address 0x080F8000: ", (int) w);	
+	memDump((char *) 0x080F8000, 4);
         printf("\n");
 		
 }
