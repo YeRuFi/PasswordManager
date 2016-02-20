3d35710d50bc3fcae0621a9d423ce80ab1f63f96
Jan Fischer <jan.fischer@tum.de>
2016-02-20 19:00:56 +0100

    [First Test] implemented the storeData function and changed main to test it.
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- PasswordManager.cpp
+++ PasswordManager.cpp
@@ -2,10 +2,50 @@
 #include <string>
 #include <iostream>
 #include <FlashDriver.h>
+#include <PasswordManager.h>
 
 
 using namespace miosix;
+using namespace std;
 
-//PasswordManager::PasswordManager();
+
+/*PasswordManager::PasswordManager(): 
+	address(0x080F8000){}
+*/
+
+void PasswordManager::startUI()
+{
+	string command="help";	
+	cout<<"Welcome to the Password Manager\n";
+	
+	//cout<<"Loading data... please wait.";
+	//PasswordManager::loadData();
+	//cout<<"Please enter the master password: \n";
+	cin>>command;
+	
+}
+
+bool PasswordManager::storeData()
+{
+	bool success = false; //return value, true when erase and all writes have been successful	
+
+	//get flash driver instance (singleton)	
+	FlashDriver& flashdriver = FlashDriver::instance();
+	
+	//erase the sector to store password data 
+	success = flashdriver.erase(flashdriver.getSectorNumber(ADDRESS)); //necessary for flash (all is put to 0xFF)
+
+	char* ramAddress = (char*) passwords; //get address of first array element
+	
+	//write char array of length sizeof(WPTuple)*arraylength to flash	
+	success &= flashdriver.write(ADDRESS, ramAddress, 64*510);
+
+	ramAddress = (char*) &checksum; //get address of checksum
+	
+	//write char array of length sizeof(int) to flash
+	success &= flashdriver.write(ADDRESS+64*510, ramAddress, sizeof(int));
+	return success;
+}
+
 	
 //}
--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <miosix.h>
-#include <stdio.h>
-#include <FlashDriver.h>
+#include <iostream>
+#include <PasswordManager.h>
 #include <string>
 
 using namespace miosix;
-FlashDriver& flashdriver = FlashDriver::instance();
-unsigned int startAddress;
 
-bool writePassword(char c)
+void writeTest()
 {
-	//FlashDriver& flashdriver = FlashDriver::instance();
-	//startAddress= flashdriver.getSectorAddress(flashdriver.getStartSector());
-	//flashdriver.erase(flashdriver.getSectorNumber(0xF8000)); //wrong address format?	
-	//flashdriver.erase(); //destroys os?
-	flashdriver.erase(flashdriver.getSectorNumber(0x080F8000)); //correct flash address
-	return flashdriver.write(0x080F8000,c); //parameters are address to write to and char to write
+	PasswordManager pwm;
+	/*string web, pas;
+	cout<<"Website: \n";
+	cin>>web;
+	cout<<"Password: \n";	
+	cin>>pas;	
+	//pwm.addPassword(web,pas);*/
+	strcpy(pwm.passwords[0].website, "Hello");
+	if(pwm.storeData())
+		cout<<"Succesful";
+	memDump((char *) 0x080F8000, 9);
 }
 
 int main()
 {
-	char test= 'p';	
-	printf("Enter character to be written to address 0x080F8000: ");     
-	scanf("%c", &test);	     
-	bool w = writePassword(test);
-	printf("Write result: %i,Character read at address 0x080F8000: ", (int) w);	
-	memDump((char *) 0x080F8000, 4);
-        printf("\n");
-		
+	writeTest(); //only for testing	
+	
+	//create PasswordManager on stack	
+	PasswordManager pwm;
+	pwm.startUI(); //start the ui of the Passwordmanager
+	
 }
