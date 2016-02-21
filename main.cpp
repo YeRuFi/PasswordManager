8f355cce39d670231a38a85c57f0c58932bf063f
Jan Fischer <jan.fischer@tum.de>
2016-02-21 18:02:58 +0100

    [First successful write test]
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <miosix.h>
-#include <iostream>
+//#include <iostream>
 #include <PasswordManager.h>
 #include <string>
+#include <stdio.h>
 
 using namespace miosix;
-
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!all functions and attributes are public for testing!!!!!!!!!
 void writeTest()
 {
-	PasswordManager pwm;
+	printf("WriteTest");	
+	char p;	
+	scanf("%c",&p);	//breakpoint
+	//printf("WriteTestprintf");
+	PasswordManager *pwm = new PasswordManager();
 	/*string web, pas;
 	cout<<"Website: \n";
 	cin>>web;
 	cout<<"Password: \n";	
 	cin>>pas;	
 	//pwm.addPassword(web,pas);*/
-	strcpy(pwm.passwords[0].website, "Hello");
-	if(pwm.storeData())
-		cout<<"Succesful";
+	
+	strcpy(pwm->passwords[0].website, "Hello");
+	if(pwm->storeData())
+		printf("Succesful");
 	memDump((char *) 0x080F8000, 9);
 }
 
 int main()
 {
 	writeTest(); //only for testing	
-	
+	printf("Does it work?"); //works with scanf at the end
+	char p;	
+	scanf("%c",&p);	
+	//cout<<"Hello";
 	//create PasswordManager on stack	
-	PasswordManager pwm;
-	pwm.startUI(); //start the ui of the Passwordmanager
+	//PasswordManager pwm;
+	//pwm.startUI(); //start the ui of the Passwordmanager
 	
 }
