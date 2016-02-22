361ef11dcfdf5627cdabb306725f870a2fdb7b58
Jan Fischer <jan.fischer@tum.de>
2016-02-22 11:37:24 +0100

    [MultipleUser] Added \n in strings and completing class by calling constructor and startUI of PasswordManager
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- SlotPasswordManager.cpp
+++ SlotPasswordManager.cpp
@@ -11,21 +11,31 @@ void SlotPasswordManager::startUI()
 	char* input = new char[32];
 	do
 	{
-		printf("Welcome to the Password Manager with multiple slots!");
+		printf("\nWelcome to the Password Manager with multiple slots!\n");
 		askinput: 
-		printf("Please choose your slot or type help");
+		printf("Please choose your slot or type help: \n");
 		scanf("%s",input);
 		if(input[0]=='h')
 		{
 			printf(HELP);
+			printf("\n");
 			goto askinput;
 		}
 		else if(input[0]=='1')
-			printf("1");
-			//pwm= new PasswordManager(ADDRESS1);
-		
+			pwm= new PasswordManager(ADDRESS1);
+		     else if(input[0]=='2')
+				pwm= new PasswordManager(ADDRESS2);
+			else if(input[0]=='3')
+				pwm= new PasswordManager(ADDRESS3);
+			     else if(input[0]=='4')
+					pwm= new PasswordManager(ADDRESS4);
+				  else {
+						printf("Invalid input... \n");
+						goto askinput;
+				       }
+		pwm.startUI();
 
-		printf("Press enter to shutdown, type restart to login again");		
+		printf("Press enter to shutdown, type restart to login again\n");		
 		scanf("%s",input);
 	} while(input!="");
 	shutdown();
