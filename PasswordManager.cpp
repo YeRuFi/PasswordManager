7b989d73200c767b4479af29f3c37c9dc2f1a521
Jan Fischer <jan.fischer@tum.de>
2016-02-23 11:59:18 +0100

    [storeData] adjusted version: write the char* of encrypted data, not the WPTuple array. Further a comment for the memory layout has been added.
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- PasswordManager.cpp
+++ PasswordManager.cpp
@@ -24,25 +24,41 @@ void PasswordManager::startUI()
 		
 }
 
+/**
+ * Flash Layout for store and load Data functions
+ * standard start address is used as example:
+ *
+ * Address 	Content 	Comment
+ * 0x080F8000	'P'		Identification
+ * 0x080F8001	'W'		"
+ * 0x080F8002	'M'		"
+ * 0x080F8003	0x00		"
+ * 0x080F8004	numOfPass	short
+ * 0x080F8006	passwords 	encryptedData
+ * 0x080F8007	"		"
+ * ...		...		...
+ * 0x080F8006+numOfPass*64 +16 //64 for every password, 16 for the checksum
+ * Afterwards	0xFF		"
+ * ...		...		... //empty area reserved for new passwords
+ * 0x080FFFFF	0xFF		"
+ *
+ */
 bool PasswordManager::storeData()
 {
 	bool success = false; //return value, true when erase and all writes have been successful	
-
-	//get flash driver instance (singleton)	
+	// identifier to identify first use and to make sure invalid data is never read coincidentally:
+	char ident[4] = "PWM"; 
+	//get flash driver instance (singleton):
 	FlashDriver& flashdriver = FlashDriver::instance();
-	
-	//erase the sector to store password data 
-	success = flashdriver.erase(flashdriver.getSectorNumber(ADDRESS)); //necessary for flash (all is put to 0xFF)
-
-	char* ramAddress = (char*) passwords; //get address of first array element
-	
-	//write char array of length sizeof(WPTuple)*arraylength to flash	
-	success &= flashdriver.write(ADDRESS, ramAddress, 64*510);
-
-	ramAddress = (char*) &checksum; //get address of checksum
-	
-	//write char array of length sizeof(int) to flash
-	success &= flashdriver.write(ADDRESS+64*510, ramAddress, sizeof(int));
+	//erase the sector to store password data:
+	success = flashdriver.erase(flashdriver.getSectorNumber(address)); //necessary for flash (all is put to 0xFF)
+	//write identifier to the flash:
+	success &= flashdriver.write(address, ident, 4);
+	char* ramAddress = (char*) &numOfPass; //get address of numOfPass
+	//write numOfPass of length 2 (short) to the flash:	
+	success &= flashdriver.write(address+4, ramAddress, 2);
+	//write char array of length sizeof(WPTuple)*numOfPass to flash
+	success &= flashdriver.write(address+6, ramAddress, PASSWORDLENGTH*2*numOfPass);
 	return success;
 }
 
