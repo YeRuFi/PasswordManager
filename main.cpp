4a953120773259b9777c2fcb6becbb66578f04d6
Jan Fischer <jan.fischer@tum.de>
2016-03-09 17:47:40 +0100

    [Added] Test for read and write. No errors found in testing.
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <miosix.h>
-//#include <iostream>
 #include <PasswordManager.h>
 #include <string>
 #include <stdio.h>
@@ -21,15 +20,80 @@ void writeTest()
 	memDump((char *) 0x080F8038, 10);
 }
 
+void readTest()
+{
+	printf("ReadTest\n");	
+	char p;	
+	scanf("%c",&p);	//breakpoint
+	char* data= new char[60];
+	strcpy(data, "PWM");
+	strcpy(data+4, "!");
+	//strcpy(data+5, ""); //0 as string terminator 
+	strcpy(data+6, "Google");
+	strcpy(data+38, "password123");
+	
+	FlashDriver& flashdriver = FlashDriver::instance();
+	flashdriver.erase(11);
+	flashdriver.write(0x080F8000,data,60);
+
+	PasswordManager *pwm = new PasswordManager();
+	pwm->loadData();
+	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
+	printf("First website: %s \n", pwm-> encryptedData);
+	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
+}
+
+void writeReadTest()
+{
+	printf("Read and write Test\n");	
+	char p;	
+	scanf("%c",&p);	//breakpoint
+	char* data= new char[60];
+	strcpy(data, "PWM");
+	strcpy(data+4, "!");
+	//strcpy(data+5, ""); //0 as string terminator 
+	strcpy(data+6, "Google");
+	strcpy(data+38, "password123");
+	
+	FlashDriver& flashdriver = FlashDriver::instance();
+	flashdriver.erase(11);
+	flashdriver.write(0x080F8000,data,60);
+
+	PasswordManager *pwm = new PasswordManager();
+	pwm->loadData();
+	printf("After First Load:\n");	
+	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
+	printf("First website: %s \n", pwm-> encryptedData);
+	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
+	
+	pwm->numOfPass=2;
+	strcpy(((char*)(pwm->encryptedData))+64, "WebMail");
+	strcpy(((char*)(pwm->encryptedData))+96, "longsafeveryandwordforPassthis");
+	if(!pwm->storeData())
+		printf("1st store not Succesful\n");
+	printf("After First Store:\n");	
+	memDump((char *) 0x080F8000, 140);
+	
+	pwm->loadData();
+	printf("After 2nd Load:\n");	
+	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
+	printf("First website: %s \n", pwm-> encryptedData);
+	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
+	printf("Second website: %s \n", pwm-> encryptedData+64);
+	printf("Second password: %s \n", (pwm-> encryptedData)+ 96);
+	
+	if(!pwm->storeData())
+		printf("2nd store not Succesful\n");
+	printf("After Second Store:\n");	
+	memDump((char *) 0x080F8000, 140);
+	
+}
+
+
 int main()
 {
-	writeTest(); //only for testing	
+	writeReadTest(); //call test function
 	printf("Does it work?"); //works with scanf at the end
 	char p;	
-	scanf("%c",&p);	
-	//cout<<"Hello";
-	//create PasswordManager on stack	
-	//PasswordManager pwm;
-	//pwm.startUI(); //start the ui of the Passwordmanager
-	
+	scanf("%c",&p);			
 }
