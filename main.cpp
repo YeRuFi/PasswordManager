595680e84c0f1bd617434d2601743ec03107c994
Jan Fischer <jan.fischer@tum.de>
2016-03-17 22:47:39 +0100

    [Added] Licence text [deleted] main.cpp unnecessary includes and commented tests
    Signed-off-by: Jan Fischer <jan.fischer@tum.de>

--- main.cpp
+++ main.cpp
@@ -1,116 +1,28 @@
+/***************************************************************************
+ *   Passsword Manager firmware for STM32F407 board using miosix kernel.   *
+ *									   *
+ *   Copyright (C) 2016 by                                                 * 
+ *          Rose Vinay Kumar Yelluri, Jan Fischer, Arlind Rufi		   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ *									   *
+ *   Refer to <http://www.github.com/YeRuFi/PasswordManager/>              *
+ ***************************************************************************/ 
+
 #include <miosix.h>
-#include <PasswordManager.h>
-#include <stdio.h>
 #include <SlotPasswordManager.h>
 using namespace miosix;
-
-/*
-//test functions need everything public in PasswordManager
-void writeTest()
-{
-	printf("WriteTest\n");	
-	char p;	
-	scanf("%c",&p);	//breakpoint
-	PasswordManager *pwm = new PasswordManager();
-	pwm->numOfPass=20;
-	strcpy(((char*)(pwm->encryptedData))+50, "Hello");
-	if(!pwm->storeData())
-		printf("Not Succesful\n");
-	memDump((char *) 0x080F8000, 9);
-	memDump((char *) 0x080F8030, 10);
-	memDump((char *) 0x080F8038, 10);
-}
-
-void readTest()
-{
-	printf("ReadTest\n");	
-	char p;	
-	scanf("%c",&p);	//breakpoint
-	char* data= new char[60];
-	strcpy(data, "PWM");
-	strcpy(data+4, "!");
-	//strcpy(data+5, ""); //0 as string terminator 
-	strcpy(data+6, "Google");
-	strcpy(data+38, "password123");
-	
-	FlashDriver& flashdriver = FlashDriver::instance();
-	flashdriver.erase(11);
-	flashdriver.write(0x080F8000,data,60);
-
-	PasswordManager *pwm = new PasswordManager();
-	pwm->loadData();
-	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
-	printf("First website: %s \n", pwm-> encryptedData);
-	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
-}
-
-void writeReadTest()
-{
-	printf("Read and write Test\n");	
-	char p;	
-	scanf("%c",&p);	//breakpoint
-	char* data= new char[60];
-	strcpy(data, "PWM");
-	strcpy(data+4, "!");
-	//strcpy(data+5, ""); //0 as string terminator 
-	strcpy(data+6, "Google");
-	strcpy(data+38, "password123");
-	
-	FlashDriver& flashdriver = FlashDriver::instance();
-	flashdriver.erase(11);
-	flashdriver.write(0x080F8000,data,60);
-
-	PasswordManager *pwm = new PasswordManager();
-	pwm->loadData();
-	printf("After First Load:\n");	
-	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
-	printf("First website: %s \n", pwm-> encryptedData);
-	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
-	
-	pwm->numOfPass=2;
-	strcpy(((char*)(pwm->encryptedData))+64, "WebMail");
-	strcpy(((char*)(pwm->encryptedData))+96, "longsafeveryandwordforPassthis");
-	if(!pwm->storeData())
-		printf("1st store not Succesful\n");
-	printf("After First Store:\n");	
-	memDump((char *) 0x080F8000, 140);
-	
-	pwm->loadData();
-	printf("After 2nd Load:\n");	
-	printf("Number of stored Passwords: %i \n", pwm->numOfPass);
-	printf("First website: %s \n", pwm-> encryptedData);
-	printf("First password: %s \n", (pwm-> encryptedData)+ 32);
-	printf("Second website: %s \n", pwm-> encryptedData+64);
-	printf("Second password: %s \n", (pwm-> encryptedData)+ 96);
-	
-	if(!pwm->storeData())
-		printf("2nd store not Succesful\n");
-	printf("After Second Store:\n");	
-	memDump((char *) 0x080F8000, 140);
-	
-}
-//test after merging encryption with flash
-void test(){
-PasswordManager *pwm = new PasswordManager();
-pwm->addPassword("google","123");
-pwm->addPassword("amazon","secondPass");
-char password[32];
-pwm->printAll();
-printf("Put the password:\n");
-scanf("%s",password);
-pwm->createKey(password);
-pwm->encrypt();
-pwm->storeData();
-
-if(!pwm->storeData())
-		printf("Error while storing\n");
-	printf("Flash content:\n");	
-	memDump((char *) 0x080F8000, 140);
-pwm->loadData();
-pwm->decrypt(pwm->encryptedData);
-pwm->printAll();
-}
-*/
 
 int main()
 {
